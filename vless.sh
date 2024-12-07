5bd98e6e327e3ee0dcc71375127128b0982c15ea
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-07 21:01:16 +0800

    更新 vless.sh

--- vless.sh
+++ vless.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 export UUID=${UUID:-'fc44fe6a-f083-4591-9c03-f8d61dc3907f'}
-export NEZHA_SERVER=${NEZHA_SERVER:-'nezha.leung0108.us.kg'} 
-export NEZHA_PORT=${NEZHA_PORT:-'443'}     
-export NEZHA_KEY=${NEZHA_KEY:-''} 
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}   
 export ARGO_AUTH=${ARGO_AUTH:-''}    
 export CFIP=${CFIP:-'dns.leung0108.us.kg'} 
