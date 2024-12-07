07161080361da673b5f1027f85f0ef40eef9283e
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-07 22:44:48 +0800

    更新 restart_agent.sh

--- restart_agent.sh
+++ restart_agent.sh
@@ -5,5 +5,5 @@ pgrep -x "nezha-agent" > /dev/null
 
 # 如果没有运行，则启动 agent
 if [ $? -ne 0 ]; then
-    nohup /home/${USER}/.nezha-agent/start.sh >/dev/null 2>&1 &
+    nnohup ./vless/start.sh >/dev/null 2>&1 &
 fi
