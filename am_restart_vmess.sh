91ae0b06d14b02b6713fe7e288be0b93f8c206d5
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-07 20:36:44 +0800

    更新 am_restart_vmess.sh

--- am_restart_vmess.sh
+++ am_restart_vmess.sh
@@ -18,7 +18,7 @@ if [ $? -ne 0 ]; then
     case $INPUT_PARAM in
         # 如果是端口数字，启动第一个命令
         [0-9]*)
-            nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile /home/${USER}/.vmess/boot.log --loglevel info --url http://localhost:$INPUT_PARAM >/dev/null 2>&1 &
+            nohup ./vless/node tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ./vless/boot.log --loglevel info --url http://localhost:$INPUT_PARAM >/dev/null 2>&1 &
 			#nohup ./vless/node tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile /home/${USER}/.vmess/boot.log --loglevel info --url http://localhost:$INPUT_PARAM > /home/${USER}/.vmess/bottest.log 2>&1 &
             ;;
         # 如果输入的参数是JSON格式，执行json固定隧道保活命令
@@ -26,11 +26,11 @@ if [ $? -ne 0 ]; then
             echo "$INPUT_PARAM" | jq empty >/dev/null 2>&1
             if [ $? -eq 0 ]; then
                 # JSON格式的内容
-                nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --config tunnel.yml run >/dev/null 2>&1 &
+                nohup ./vless/node tunnel --edge-ip-version auto --config tunnel.yml run >/dev/null 2>&1 &
 				#nohup ./vless/node tunnel --edge-ip-version auto --config tunnel.yml run > /home/${USER}/.vmess/bottest.log 2>&1 &
             else
                 # token固定隧道保活命令
-                nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token "$INPUT_PARAM" >/dev/null 2>&1 &
+                nohup ./vless/node tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token "$INPUT_PARAM" >/dev/null 2>&1 &
                 #nohup ./vless/node tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token "$INPUT_PARAM" > /home/${USER}/.vmess/bottest.log 2>&1 &
             fi
             ;;
