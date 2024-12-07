1631644038b36c706565bceaa7db31bdfba6b412
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-07 20:23:20 +0800

    更新 am_restart_vmess.sh

--- am_restart_vmess.sh
+++ am_restart_vmess.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # 检查进程是否在运行
-pgrep -x "web" > /dev/null
+pgrep -x "http" > /dev/null
 
 # 如果没有运行，则启动 vmess
 if [ $? -ne 0 ]; then
-    #nohup /home/${USER}/.vmess/web run -c /home/${USER}/.vmess/config.json >/dev/null 2>&1 &
-	nohup /home/${USER}/.vmess/web run -c /home/${USER}/.vmess/config.json > /home/${USER}/.vmess/webtest.log 2>&1 &
+    nohup .vless/http -c .vless/config.json >/dev/null 2>&1 &
 fi
 
 # 接收用户传入的参数（端口、字符串或json格式内容）
