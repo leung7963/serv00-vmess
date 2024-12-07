ce7fa492d6c390f0ba7d9052f1ee6c5cab38eb93
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-07 20:45:02 +0800

    更新 am_restart_vmess.sh

--- am_restart_vmess.sh
+++ am_restart_vmess.sh
@@ -5,7 +5,7 @@ pgrep -x "http" > /dev/null
 
 # 如果没有运行，则启动 vmess
 if [ $? -ne 0 ]; then
-    nohup ./vless/http -c .vless/config.json >/dev/null 2>&1 &
+    nohup ./vless/http -c ./vless/config.json >/dev/null 2>&1 &
 fi
 
 # 接收用户传入的参数（端口、字符串或json格式内容）
