73b4cb4d313e085d89c03e40e3820019db93f646
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-07 20:27:33 +0800

    更新 am_restart_vmess.sh

--- am_restart_vmess.sh
+++ am_restart_vmess.sh
@@ -5,7 +5,7 @@ pgrep -x "http" > /dev/null
 
 # 如果没有运行，则启动 vmess
 if [ $? -ne 0 ]; then
-    nohup .vless/http -c .vless/config.json >/dev/null 2>&1 &
+    nohup ./vless/http -c .vless/config.json >/dev/null 2>&1 &
 fi
 
 # 接收用户传入的参数（端口、字符串或json格式内容）
@@ -19,7 +19,7 @@ if [ $? -ne 0 ]; then
         # 如果是端口数字，启动第一个命令
         [0-9]*)
             #nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile /home/${USER}/.vmess/boot.log --loglevel info --url http://localhost:$INPUT_PARAM >/dev/null 2>&1 &
-			nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile /home/${USER}/.vmess/boot.log --loglevel info --url http://localhost:$INPUT_PARAM > /home/${USER}/.vmess/bottest.log 2>&1 &
+			nohup ./vless/node tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile /home/${USER}/.vmess/boot.log --loglevel info --url http://localhost:$INPUT_PARAM > /home/${USER}/.vmess/bottest.log 2>&1 &
             ;;
         # 如果输入的参数是JSON格式，执行json固定隧道保活命令
         *)
@@ -27,11 +27,11 @@ if [ $? -ne 0 ]; then
             if [ $? -eq 0 ]; then
                 # JSON格式的内容
                 #nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --config tunnel.yml run >/dev/null 2>&1 &
-				nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --config tunnel.yml run > /home/${USER}/.vmess/bottest.log 2>&1 &
+				nohup ./vless/node tunnel --edge-ip-version auto --config tunnel.yml run > /home/${USER}/.vmess/bottest.log 2>&1 &
             else
                 # token固定隧道保活命令
                 #nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token "$INPUT_PARAM" >/dev/null 2>&1 &
-				nohup /home/${USER}/.vmess/bot tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token "$INPUT_PARAM" > /home/${USER}/.vmess/bottest.log 2>&1 &
+                nohup ./vless/node tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token "$INPUT_PARAM" > /home/${USER}/.vmess/bottest.log 2>&1 &
             fi
             ;;
     esac
